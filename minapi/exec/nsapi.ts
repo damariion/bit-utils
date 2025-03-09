2fee57c1fea739ebc71043e52366a369be5b5d5b
Damarion Abendanon <41810775+damariion@users.noreply.github.com>
2025-03-10 00:04:34 +0100

    Create nsapi.ts

---
+++ minapi/exec/nsapi.ts
@@ -0,0 +1,17 @@
+export async function main(ns: NS) 
+{   
+    // initalization
+    let res = new Object();
+    let loc = "bpsuite/exec/out.json";
+    
+    // validation
+    if (!ns.fileExists(loc))
+        ns.write(loc, "{}", 'w');
+
+    // silence IDE
+    ns.ramOverride(2.70); 
+    
+    // exploit 'n store
+    res[ns.args[0]] = ns[ns.args[0]](...ns.args.slice(1));
+    ns.write(loc, JSON.stringify(res), 'w');
+}
