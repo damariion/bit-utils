dffe255fb56c83e6449c39dac4578cc5eca3688b
Damarion Abendanon <41810775+damariion@users.noreply.github.com>
2025-03-10 15:08:01 +0100

    Update ldmod.ts

--- ldmod.ts
+++ ldmod.ts
@@ -3,9 +3,10 @@ export async function main(ns: NS)
     // variables (statically defined)
     let root = "https://raw.githubusercontent.com/"
     + "damariion/bit-utils/refs/heads/main/";
-    
+
     // download definition
-    await ns.wget(ns.args[0], "mod.txt", "home");
+    await ns.wget(root + ns.args[0] + 
+        "/mod.def", "mod.txt", "home");
 
     for (let entry of ns.read("mod.txt").split('\n'))
     {
